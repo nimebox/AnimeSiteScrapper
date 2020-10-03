aad75191c560c033675c391b4e47dc75bce04519
pcpl2 <kontakt@patryk-lawicki.pl>
2020-10-03 03:49:07 +0200

    [Main] Removed old models.

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -1,35 +1,7 @@
 import java.time.ZonedDateTime
 import java.util.UUID
 
-import org.apache.solr.common.SolrInputDocument
-
 object Models {
-
-    case class AnimeEpisodePlayerSorl( playerName: String, url: String ) {
-        def toSolrDocument( episodeId: UUID ): SolrInputDocument = {
-            val doc = new SolrInputDocument()
-            doc.addField( "id", UUID.randomUUID().toString )
-            doc.addField( "episodeId", episodeId.toString )
-            doc.addField( "playerName", playerName )
-            doc.addField( "url", url )
-            doc
-        }
-    }
-
-    case class AnimeEpisodeSorl( title: String, url: String, players: Array[ AnimeEpisodePlayerSorl ] ) {
-        def toSolrDocument( animeId: UUID ): (SolrInputDocument, UUID) = {
-            val doc = new SolrInputDocument()
-            val epId = UUID.randomUUID()
-            doc.addField( "id", epId.toString )
-            doc.addField( "animeId", animeId.toString )
-            doc.addField( "title", title )
-            doc.addField( "url", url )
-            (doc, epId)
-        }
-    }
-
-    case class AnimeSorl( id: UUID, title: String, url: String, imageUrl: Option[ String ] = None, eps: Array[ AnimeEpisodeSorl ] )
-
     case class AnimePage( title: String, url: String, imageB64: Option[ String ] = None )
 
     case class AnimePageEpisode( title: String, url: String, players: Set[ AnimePagePlayer ] )
